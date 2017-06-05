6480cc603bcb0321c31cbfe0d3fe1e1d88f42742
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-06-05 22:23:53 +0800

    apache-php-mysql_0.1

---
+++ apache-php-mysql/0.1/README.md
@@ -0,0 +1,53 @@
+# Docker Image for Apache-PHP-MySQL
+## Overview
+This Docker image is built for [Azure Web App on Linux](https://docs.microsoft.com/en-us/azure/app-service-web/app-service-linux-intro).
+
+## Components
+This docker image contains the following components:
+
+1. PHP          **7.1.2**
+2. Apache HTTPD **2.4.25**
+3. MariaDB      **10.0+**
+4. phpMyAdmin   **4.6.6**
+
+Ubuntu 16.04 is used as the base image.
+
+## Features
+This docker image enables you to:
+
+- run a Apache/PHP/MySQL Environment on **Azure Web App on Linux**;
+- connect your App site to **Azure ClearDB** or the builtin MariaDB;
+- manage the build-in MariaDB with the built-in phpMyAdmin;
+
+## Deploying / Running
+You can specify the following environment variables when deploying the image to Azure or running it on your Docker engine's host.
+
+Name | Default Value
+---- | -------------
+DATABASE_NAME | appdb
+DATABASE_USERNAME | appuser
+DATABASE_PASSWORD | MS173m_QN
+PHPMYADMIN_USERNAME | phpmyadmin
+PHPMYADMIN_PASSWORD | MS173m_QN
+
+### Deploying to Azure
+With the button below, you can easily deploy the image to Azure.
+
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
+## The Builtin MariaDB server
+The builtin MariaDB server uses port 3306.
+
+## The Builtin phpMyAdmin Site
+You can access the builtin phpMyAdmin site with a URL like below if you're using the build-in MariaDB:
+
+**http://hostname[:port]/phpmyadmin**
+
+## How to install APP
+1. Use any FTP tool you prefer to connect to the site (you can get the credentials on Azure portal);
+2. Upload the files of the app that you want to install to the folder /home/site/wwwroot/;
+3. Create database on the built-in MariaDB database for your app with the build-in phpMyAdmin;
+4. Update the config file of your app with your created database information;
+
+## Startup Log
+The startup log file (**entrypoint.log**) is placed under the folder /home/LogFiles.
---
+++ apache-php-mysql/0.1/entrypoint.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+log(){
+	while read line ; do
+		echo "`date '+%D %T'` $line"
+	done
+}
+
+set -e
+logfile=/home/LogFiles/entrypoint.log
+test ! -f $logfile && mkdir -p /home/LogFiles && touch $logfile
+exec > >(log | tee -ai $logfile)
+exec 2>&1
+
+set_var_if_null(){
+	local varname="$1"
+	if [ ! "${!varname:-}" ]; then
+		export "$varname"="$2"
+	fi
+}
+
+setup_mariadb_data_dir(){
+	test ! -d "$MARIADB_DATA_DIR" && echo "INFO: $MARIADB_DATA_DIR not found. creating ..." && mkdir -p "$MARIADB_DATA_DIR"
+
+	# check if 'mysql' database exists
+	if [ ! -d "$MARIADB_DATA_DIR/mysql" ]; then
+		echo "INFO: 'mysql' database doesn't exist under $MARIADB_DATA_DIR. So we think $MARIADB_DATA_DIR is empty."
+		echo "Copying all data files from the original folder /var/lib/mysql to $MARIADB_DATA_DIR ..."
+		cp -nR /var/lib/mysql/. $MARIADB_DATA_DIR
+	else
+		echo "INFO: 'mysql' database already exists under $MARIADB_DATA_DIR."
+	fi
+
+	rm -rf /var/lib/mysql
+	ln -s $MARIADB_DATA_DIR /var/lib/mysql
+
+	chown -R mysql:mysql $MARIADB_DATA_DIR
+}
+
+start_mariadb(){
+	service mysql start
+	rm -f /tmp/mysql.sock
+	ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
+}
+
+setup_phpmyadmin(){
+	test ! -d "$PHPMYADMIN_HOME" && echo "INFO: $PHPMYADMIN_HOME not found. creating ..." && mkdir -p "$PHPMYADMIN_HOME"
+
+	cd $PHPMYADMIN_HOME
+	mv $PHPMYADMIN_SOURCE/phpmyadmin.tar.gz $PHPMYADMIN_HOME/
+	tar -xf phpmyadmin.tar.gz -C $PHPMYADMIN_HOME --strip-components=1
+	# create config.inc.php
+	cp -nR $PHPMYADMIN_SOURCE/phpmyadmin-config.inc.php $PHPMYADMIN_HOME/config.inc.php
+	rm $PHPMYADMIN_HOME/phpmyadmin.tar.gz
+	rm -rf $PHPMYADMIN_SOURCE
+
+	chown -R www-data:www-data $PHPMYADMIN_HOME
+}
+
+update_settings(){
+	set_var_if_null "DATABASE_NAME" "appdb"
+	set_var_if_null "DATABASE_USERNAME" "appuser"
+	set_var_if_null "DATABASE_PASSWORD" "MS173m_QN"
+	set_var_if_null 'PHPMYADMIN_USERNAME' 'phpmyadmin'
+	set_var_if_null 'PHPMYADMIN_PASSWORD' 'MS173m_QN'
+}
+
+set -e
+
+update_settings
+
+echo "INFO: DATABASE_NAME:" $DATABASE_NAME
+echo "INFO: DATABASE_USERNAME:" $DATABASE_USERNAME
+echo "INFO: PHPMYADMIN_USERNAME:" $PHPMYADMIN_USERNAME
+
+test ! -d "$HTTPD_LOG_DIR" && echo "INFO: $HTTPD_LOG_DIR not found. creating ..." && mkdir -p "$HTTPD_LOG_DIR"
+chown -R www-data:www-data $HTTPD_LOG_DIR
+apachectl start
+
+# local MariaDB is used 
+echo "Setting up MariaDB data dir ..."
+setup_mariadb_data_dir
+echo "Setting up MariaDB log dir ..."
+test ! -d "$MARIADB_LOG_DIR" && echo "INFO: $MARIADB_LOG_DIR not found. creating ..." && mkdir -p "$MARIADB_LOG_DIR"
+chown -R mysql:mysql $MARIADB_LOG_DIR
+echo "Starting local MariaDB ..."
+start_mariadb
+
+if [ ! -e "$PHPMYADMIN_HOME/config.inc.php" ]; then
+	echo "Granting user for phpMyAdmin ..."
+	mysql -u root -e "GRANT ALL ON *.* TO \`$PHPMYADMIN_USERNAME\`@'localhost' IDENTIFIED BY '$PHPMYADMIN_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+	echo "Creating database if not exists ..."
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`$DATABASE_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
+	echo "Granting user ..."
+	mysql -u root -e "GRANT ALL ON \`$DATABASE_NAME\`.* TO \`$DATABASE_USERNAME\`@\`$DATABASE_HOST\` IDENTIFIED BY '$DATABASE_PASSWORD'; FLUSH PRIVILEGES;"
+
+	echo "INFO: $PHPMYADMIN_HOME/config.inc.php not found."
+	echo "Installing phpMyAdmin ..."
+	setup_phpmyadmin
+	else
+	echo "INFO: $PHPMYADMIN_HOME/config.inc.php already exists."
+fi
+
+echo "Loading phpMyAdmin conf ..."
+if ! grep -q "^Include conf/httpd-phpmyadmin.conf" $HTTPD_CONF_FILE; then
+	echo 'Include conf/httpd-phpmyadmin.conf' >> $HTTPD_CONF_FILE
+fi
+
+apachectl stop
+# delay 2 seconds to try to avoid "httpd (pid XX) already running"
+sleep 2s
+
+echo "Starting Apache httpd -D FOREGROUND ..."
+apachectl start -D FOREGROUND
